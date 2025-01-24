6b224f5aa6df652438c0ae64052cf5daceac23f1
Vivek Anand <thevivek9939@gmail.com>
2025-01-24 16:29:59 +0530

    bacend url updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Orders from './pages/Orders/Orders'
 import { ToastContainer } from 'react-toastify';
 
 const App = () => {
-  const url = "http://localhost:10";
+  const url = "https://backend-j819.onrender.com";
   return (
     <div>
       <ToastContainer/>
